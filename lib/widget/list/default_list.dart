ebfd224af7968460ee1ed1ef5e23bec765879374
drafting_dreams <draftingdreams666@gmail.com>
2019-07-07 17:34:48 +0800

    Add collapse function on default and month list

--- lib/widget/list/default_list.dart
+++ lib/widget/list/default_list.dart
@@ -5,15 +5,27 @@ import 'package:provider/provider.dart';
 import 'package:money_book/screens/expense_edit_screen.dart';
 import 'package:money_book/screens/income_edit_screen.dart';
 import 'package:money_book/api/transaction.dart';
-import 'package:money_book/model/transaction.dart';
 
 enum DeleteType { NORMAL, CASCADE }
 
-class DefaultList extends StatelessWidget {
+class DefaultList extends StatefulWidget {
   final Function onRefresh;
-  final SlidableController slidableController = SlidableController();
 
   DefaultList(this.onRefresh);
+
+  @override
+  _DefaultListState createState() => _DefaultListState();
+}
+
+class _DefaultListState extends State<DefaultList> {
+  final SlidableController slidableController = SlidableController();
+
+  final List<Map<String, int>> hiddenMonth = [];
+
+  bool hiddenContained(int year, int month) =>
+      hiddenMonth.indexWhere((Map element) =>
+          element['year'] == year && element['month'] == month) !=
+      -1;
 
   Future<DeleteType> _deletionDialog(BuildContext context) async {
     return showDialog(
@@ -45,7 +57,7 @@ class DefaultList extends StatelessWidget {
     return RefreshIndicator(
 //      onRefresh: _onRefreshWrapper(
 //        transactions.previousLoadingReference, transactions),
-      onRefresh: onRefresh,
+      onRefresh: widget.onRefresh,
       child: ListView.builder(
           physics: const AlwaysScrollableScrollPhysics(),
           itemCount: transactions.length,
@@ -61,80 +73,112 @@ class DefaultList extends StatelessWidget {
               final totalAmount =
                   transactions.getTotalOfMonth(transaction.date);
               total = (totalAmount > 0 ? '+' : '') + totalAmount.toString();
-              leading = Container(
-                decoration: BoxDecoration(color: Colors.blue[100]),
-                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('${transaction.date.year}/${transaction.date.month}',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold)),
-                      Text(total,
-                          textAlign: TextAlign.right,
-                          style: totalAmount > 0
-                              ? TextStyle(color: Colors.green[600])
-                              : TextStyle(color: Colors.red[600]))
-                    ]),
-              );
+              leading = GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (hiddenContained(
+                          transaction.date.year, transaction.date.month)) {
+                        hiddenMonth.removeWhere((Map element) =>
+                            element['year'] == transaction.date.year &&
+                            element['month'] == transaction.date.month);
+                      } else {
+                        hiddenMonth.add({
+                          'year': transaction.date.year,
+                          'month': transaction.date.month
+                        });
+                      }
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.blue[100]),
+                    padding: EdgeInsets.fromLTRB(10, 15, 5, 15),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                              '${transaction.date.year}/${transaction.date.month}',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          Expanded(
+                            child: Container(),
+                          ),
+                          Text(total,
+                              textAlign: TextAlign.right,
+                              style: totalAmount > 0
+                                  ? TextStyle(color: Colors.green[600])
+                                  : TextStyle(color: Colors.red[600])),
+                          hiddenContained(
+                                  transaction.date.year, transaction.date.month)
+                              ? Icon(Icons.arrow_left)
+                              : Icon(Icons.arrow_drop_down)
+                        ]),
+                  ));
             }
             return Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 leading,
-                Divider(
-                  height: 2.0,
-                ),
-                Slidable(
-                  key: ValueKey(index),
-                  controller: slidableController,
-                  actionPane: SlidableDrawerActionPane(),
-                  secondaryActions: <Widget>[
-                    IconSlideAction(
-                      caption: 'Edit',
-                      color: Colors.grey[350],
-                      icon: Icons.edit,
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (BuildContext context) {
-                          if (transaction.value >= 0) {
-                            return IncomeEditScreen(id: transaction.id);
-                          }
-                          return ExpenseEditScreen(id: transaction.id);
-                        }));
-                      },
-                    ),
-                    IconSlideAction(
-                      caption: 'Delete',
-                      color: Colors.red,
-                      icon: Icons.delete,
-                      onTap: () {
-                        _deletionDialog(context).then((DeleteType type) {
-                          if (type == DeleteType.NORMAL) {
-                            TransactionAPI.delete(transaction.id);
-                            transactions.removeById(transaction.id);
-                          } else if (type == DeleteType.CASCADE) {
-                            TransactionAPI.delete(transaction.id,
-                                transaction.accountId, transaction.value);
-                            transactions.removeById(transaction.id);
-                          }
-                        });
-                      },
-                    )
-                  ],
-                  child: ListTile(
-                    title: Text('${transaction.name}'),
-                    subtitle: Text(
-                      (transaction.value > 0 ? '+' : '') +
-                          '${transaction.value}',
-                      style: transaction.value >= 0
-                          ? TextStyle(color: Colors.green[600])
-                          : TextStyle(color: Colors.red[600]),
-                    ),
-                    trailing: Text(
-                        '${transaction.date.day}/${transaction.date.month}'),
-                  ),
-                ),
+                hiddenContained(transaction.date.year, transaction.date.month)
+                    ? Container(
+                        height: 0,
+                      )
+                    : Divider(
+                        height: 2.0,
+                      ),
+                hiddenContained(transaction.date.year, transaction.date.month)
+                    ? Container(
+                        height: 0,
+                      )
+                    : Slidable(
+                        key: ValueKey(index),
+                        controller: slidableController,
+                        actionPane: SlidableDrawerActionPane(),
+                        secondaryActions: <Widget>[
+                          IconSlideAction(
+                            caption: 'Edit',
+                            color: Colors.grey[350],
+                            icon: Icons.edit,
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                if (transaction.value >= 0) {
+                                  return IncomeEditScreen(id: transaction.id);
+                                }
+                                return ExpenseEditScreen(id: transaction.id);
+                              }));
+                            },
+                          ),
+                          IconSlideAction(
+                            caption: 'Delete',
+                            color: Colors.red,
+                            icon: Icons.delete,
+                            onTap: () {
+                              _deletionDialog(context).then((DeleteType type) {
+                                if (type == DeleteType.NORMAL) {
+                                  TransactionAPI.delete(transaction.id);
+                                  transactions.removeById(transaction.id);
+                                } else if (type == DeleteType.CASCADE) {
+                                  TransactionAPI.delete(transaction.id,
+                                      transaction.accountId, transaction.value);
+                                  transactions.removeById(transaction.id);
+                                }
+                              });
+                            },
+                          )
+                        ],
+                        child: ListTile(
+                          title: Text('${transaction.name}'),
+                          subtitle: Text(
+                            (transaction.value > 0 ? '+' : '') +
+                                '${transaction.value}',
+                            style: transaction.value >= 0
+                                ? TextStyle(color: Colors.green[600])
+                                : TextStyle(color: Colors.red[600]),
+                          ),
+                          trailing: Text(
+                              '${transaction.date.day}/${transaction.date.month}'),
+                        ),
+                      ),
               ],
             );
           }),
--- lib/widget/list/month_list.dart
+++ lib/widget/list/month_list.dart
@@ -5,27 +5,33 @@ import 'package:money_book/shared_state/account.dart';
 import 'package:money_book/shared_state/transactions.dart';
 import 'package:provider/provider.dart';
 
-class MonthList extends StatelessWidget {
+class MonthList extends StatefulWidget {
   final Function refresh;
   final List<Map<String, dynamic>> monthTransactionTotalList;
 
+  MonthList(this.refresh, this.monthTransactionTotalList);
+
+  @override
+  _MonthListState createState() => _MonthListState();
+}
+
+class _MonthListState extends State<MonthList> {
+  final Set<int> hiddenYear = Set();
+
   Function onRefreshWrapper(String accountId, TransactionClass tc) {
     Future<void> _onRefresh() async {
-      if (monthTransactionTotalList.length > 0) {
+      if (widget.monthTransactionTotalList.length > 0) {
         List<Map<String, dynamic>> previous =
             await TransactionAPI.loadPreviousYear(
-                accountId, monthTransactionTotalList[0]['year'], tc);
-        refresh(previous);
+                accountId, widget.monthTransactionTotalList[0]['year'], tc);
+        widget.refresh(previous);
       }
     }
 
     return _onRefresh;
   }
 
-  MonthList(this.refresh, this.monthTransactionTotalList);
-
   TextStyle style(double total) {
-    print(total);
     return total > 0
         ? TextStyle(color: Colors.green[600])
         : total < 0 ? TextStyle(color: Colors.red[600]) : null;
@@ -39,45 +45,71 @@ class MonthList extends StatelessWidget {
             onRefreshWrapper(accountState.currentAccount.id, transactions.tc),
         child: ListView.builder(
           physics: const AlwaysScrollableScrollPhysics(),
-          itemCount: monthTransactionTotalList.length,
+          itemCount: widget.monthTransactionTotalList.length,
           itemBuilder: (BuildContext context, int index) {
-            final e = monthTransactionTotalList[index];
+            final e = widget.monthTransactionTotalList[index];
             final previous =
-                index > 0 ? monthTransactionTotalList[index - 1] : null;
+                index > 0 ? widget.monthTransactionTotalList[index - 1] : null;
             Widget leading = Container(height: 0);
             String total = '';
 
             if (previous == null || previous['year'] != e['year']) {
-              final totalAmount = monthTransactionTotalList
+              final totalAmount = widget.monthTransactionTotalList
                   .where((item) => item['year'] == e['year'])
                   .fold(0.0, (current, next) => current + next['amount']);
               total = (totalAmount > 0 ? '+' : '') + totalAmount.toString();
-              leading = Container(
-                decoration: BoxDecoration(color: Colors.blue[100]),
-                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(e['year'].toString(),
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold)),
-                      Text(total,
-                          textAlign: TextAlign.right,
-                          style: style(totalAmount))
-                    ]),
-              );
+              leading = GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (hiddenYear.contains(e['year'])) {
+                        hiddenYear.remove(e['year']);
+                      } else {
+                        hiddenYear.add(e['year']);
+                      }
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.blue[100]),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(e['year'].toString(),
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          Expanded(
+                            child: Container(),
+                          ),
+                          Text(total,
+                              textAlign: TextAlign.right,
+                              style: style(totalAmount)),
+                          hiddenYear.contains(e['year'])
+                              ? Icon(Icons.arrow_left)
+                              : Icon(Icons.arrow_drop_down)
+                        ]),
+                  ));
             }
 
             return Column(
               children: <Widget>[
                 leading,
-                Divider(height: 2.0),
-                ListTile(
-                  title: Text(Util.getMonthName(e['month'])),
-                  trailing: Text(
-                      (e['amount'] > 0 ? '+' : '') + e['amount'].toString(),
-                      style: style(e['amount'])),
-                )
+                hiddenYear.contains(e['year'])
+                    ? Container(
+                        height: 0,
+                      )
+                    : Divider(height: 2.0),
+                hiddenYear.contains(e['year'])
+                    ? Container(
+                        height: 0,
+                      )
+                    : ListTile(
+                        title: Text(Util.getMonthName(e['month'])),
+                        trailing: Text(
+                            (e['amount'] > 0 ? '+' : '') +
+                                e['amount'].toString(),
+                            style: style(e['amount'])),
+                      )
               ],
             );
           },
