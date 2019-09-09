13ac315719dac44b15819259dbff3257aba7f347
drafting_dreams <draftingdreams666@gmail.com>
2019-09-09 21:41:12 +0800

    Add date selector on bill edit screen

--- lib/screens/bill_edit_screen.dart
+++ lib/screens/bill_edit_screen.dart
@@ -16,7 +16,9 @@ class _BillEditScreen extends State<BillEditScreen> {
   bool _autoValidate = false;
   bool _autoPay = true;
   bool _repeat = false;
+  DateTime date = DateTime.now();
   String _selectedType;
+  DateTime _repeatDate = DateTime.now();
   final _amountController = TextEditingController();
   final _descriptionController = TextEditingController();
   final _frequencyController = TextEditingController();
@@ -26,10 +28,108 @@ class _BillEditScreen extends State<BillEditScreen> {
     super.initState();
     _frequencyController.text = '1';
     _repeatTimeController.text = '2';
+    // if today is after 28, then _repeatDate will be next month's first day
+    if (_repeatDate.day > 28) {
+      if (_repeatDate.month < 12) {
+        _repeatDate = DateTime(_repeatDate.year, _repeatDate.month + 1, 1);
+      } else {
+        _repeatDate = DateTime(_repeatDate.year + 1, 1, 1);
+      }
+    }
+  }
+
+  Future _selectDate() async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: date != null ? date : DateTime.now(),
+        firstDate: DateTime(2019),
+        lastDate: DateTime(DateTime.now().year + 70));
+    if (picked != null) {
+      final DateTime today = DateTime.now();
+      // bill date should be before today and can't be today !!!
+      if (picked.compareTo(today) < 0 &&
+          (!(picked.year == today.year &&
+              picked.month == today.month &&
+              picked.day == today.day))) {
+        showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => AlertDialog(
+                    title: Text('Inappropriate Bill Date'),
+                    content: Text('Bill date should be on or after today.'),
+                    actions: [
+                      FlatButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                            _selectDate();
+                          },
+                          child: Text('OK'))
+                    ]));
+      } else {
+        setState(() {
+          date = picked;
+        });
+      }
+    }
+  }
+
+  Future _selectRepeatDate() async {
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: _repeatDate != null ? _repeatDate : DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime(DateTime.now().year + 70));
+    if (picked != null) {
+      final DateTime today = DateTime.now();
+      // bill date should be before today and can't be today !!!
+      if (picked.compareTo(today) < 0 &&
+        (!(picked.year == today.year &&
+          picked.month == today.month &&
+          picked.day == today.day))) {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) => AlertDialog(
+            title: Text('Inappropriate Bill Date'),
+            content: Text('Bill date should be on or after today.'),
+            actions: [
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _selectDate();
+                },
+                child: Text('OK'))
+            ]));
+      } else if (picked.day > 28) {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) => AlertDialog(
+            title: Text('Inappropriate Bill Date'),
+            content: Text('Repeat Bill day should be on or before 28 of each month.'),
+            actions: [
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _selectRepeatDate();
+                },
+                child: Text('OK'))
+            ]));
+      } else {
+        setState(() {
+          _repeatDate = picked;
+        });
+      }
+    }
   }
 
   Widget build(BuildContext context) {
     var typeInfo = Provider.of<ExpenseTypeInfo>(context);
+    if (_selectedType == null) {
+      setState(() {
+        _selectedType = typeInfo.types[0].name;
+      });
+    }
 
     return Scaffold(
         appBar: AppBar(title: Text('Bill')),
@@ -156,6 +256,28 @@ class _BillEditScreen extends State<BillEditScreen> {
                       ),
                     ],
                   ),
+                  ExpandedSection(
+                    expand: !_repeat,
+                    child: InkWell(
+                        onTap: _selectDate,
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Row(
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(right: 15),
+                                  child: Icon(
+                                    Icons.date_range,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                                Expanded(
+                                    child: Text(
+                                        '${date.year}-${date.month}-${date.day}',
+                                        style: TextStyle(fontSize: 16)))
+                              ],
+                            ))),
+                  ),
                   Padding(
                       padding: EdgeInsets.symmetric(vertical: 5),
                       child: Row(
@@ -182,65 +304,87 @@ class _BillEditScreen extends State<BillEditScreen> {
                       )),
                   ExpandedSection(
                     expand: _repeat,
-                    child: Column(
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(right: 15.0),
-                              child: Icon(
-                                Icons.av_timer,
-                                color: Theme.of(context).primaryColor,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left:35.0),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(right: 15.0),
+                                child: Icon(
+                                  Icons.av_timer,
+                                  color: Theme.of(context).primaryColor,
+                                ),
                               ),
-                            ),
-                            Expanded(
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                    labelText: 'Repeat Frequency'),
-                                controller: _frequencyController,
-                                keyboardType: TextInputType.number,
-                                validator: (v) {
-                                  String value = _frequencyController.text;
-                                  if (value.isEmpty ||
-                                      !Util.isInt(value) ||
-                                      int.parse(value) < 1 ||
-                                      int.parse(value) > 12) {
-                                    return 'Please enter an integer ranged from 1 to 12';
-                                  }
-                                },
+                              Expanded(
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                      labelText: 'Repeat Frequency By Month'),
+                                  controller: _frequencyController,
+                                  keyboardType: TextInputType.number,
+                                  validator: (v) {
+                                    String value = _frequencyController.text;
+                                    if (value.isEmpty ||
+                                        !Util.isInt(value) ||
+                                        int.parse(value) < 1 ||
+                                        int.parse(value) > 12) {
+                                      return 'Please enter an integer ranged from 1 to 12';
+                                    }
+                                  },
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(right: 15.0),
-                              child: Icon(
-                                Icons.update,
-                                color: Theme.of(context).primaryColor,
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(right: 15.0),
+                                child: Icon(
+                                  Icons.update,
+                                  color: Theme.of(context).primaryColor,
+                                ),
                               ),
-                            ),
-                            Expanded(
-                              child: TextFormField(
-                                decoration:
-                                    InputDecoration(labelText: 'Repeat Times'),
-                                controller: _repeatTimeController,
-                                keyboardType: TextInputType.number,
-                                validator: (v) {
-                                  String value = _repeatTimeController.text;
-                                  if (value.isEmpty ||
-                                      !Util.isInt(value) ||
-                                      int.parse(value) < 2 ||
-                                      int.parse(value) > 60) {
-                                    return 'Please enter an integer ranged from 3 to 60';
-                                  }
-                                },
+                              Expanded(
+                                child: TextFormField(
+                                  decoration:
+                                      InputDecoration(labelText: 'Repeat Times'),
+                                  controller: _repeatTimeController,
+                                  keyboardType: TextInputType.number,
+                                  validator: (v) {
+                                    String value = _repeatTimeController.text;
+                                    if (value.isEmpty ||
+                                        !Util.isInt(value) ||
+                                        int.parse(value) < 2 ||
+                                        int.parse(value) > 60) {
+                                      return 'Please enter an integer ranged from 3 to 60';
+                                    }
+                                  },
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+                          InkWell(
+                            onTap: _selectRepeatDate,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                              child: Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.only(right: 15),
+                                    child: Icon(
+                                      Icons.date_range,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      '${_repeatDate.year}-${_repeatDate.month}-${_repeatDate.day}',
+                                      style: TextStyle(fontSize: 16)))
+                                ],
+                              )))
+                        ],
+                      ),
                     ),
                   )
                 ],
