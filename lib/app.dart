c83e4917d12eff24b3406c9947dc0877ac5d87a0
drafting_dreams <draftingdreams666@gmail.com>
2019-12-14 16:39:19 +0800

    feat: add firebase GA

--- lib/app.dart
+++ lib/app.dart
@@ -7,30 +7,36 @@ import 'package:provider/provider.dart';
 import 'package:money_book/shared_state/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'locale/locales.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 class App extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
   Widget build(BuildContext context) {
     final themeChanger = Provider.of<ThemeChanger>(context);
 
-    return MaterialApp(title: 'Moneybook', navigatorKey: navigatorKey,
+    return MaterialApp(
+      title: 'Moneybook',
+      navigatorKey: navigatorKey,
       theme: themeChanger.getTheme(),
       localizationsDelegates: [
         AppLocalizationsDelegate(),
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
-      supportedLocales: [
-        Locale('en', ''),
-        Locale('zh', '')
-      ],
+      supportedLocales: [Locale('en', ''), Locale('zh', '')],
       routes: {
-      '/': (context) => BookScreen(),
-      '/edit/income': (context) => IncomeEditScreen(),
-      '/edit/expense': (context) => ExpenseEditScreen(),
-      '/accounts': (context) => AccountScreen()
-    });
+        '/': (context) => BookScreen(),
+        '/edit/income': (context) => IncomeEditScreen(),
+        '/edit/expense': (context) => ExpenseEditScreen(),
+        '/accounts': (context) => AccountScreen()
+      },
+      navigatorObservers: <NavigatorObserver>[observer],
+    );
   }
-
 }
